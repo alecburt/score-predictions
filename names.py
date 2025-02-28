66bd80604cc71a20592c748320cbbc225f5cdc21
alecburt <alec_burt@yahoo.com>
2025-02-28 01:18:17 +0100

    Add files via upload
    These are example names once I start using I will need to build up a register.

---
+++ names.py
@@ -0,0 +1,26 @@
+
+def check_name(name):
+    if name == "alec":
+        team_name = name.replace("alec", "Alec Burt") 
+        return team_name
+    
+    if name == "ArmyAmarillo":
+        team_name = name.replace("ArmyAmarillo", "Alec Burt") 
+        return team_name
+        
+    if name == "ari":
+        team_name = name.replace("ari", "Ari Ordeix") 
+        return team_name
+
+    if name == "simon":
+        team_name = name.replace("simon", "Simon Furness")
+        return team_name
+    
+    if name == "dad":
+        team_name = name.replace("dad", "Norrie Burt")
+        return team_name
+    
+    else:
+        team_name = name
+        return team_name
+
