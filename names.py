914259c253e7c6f61743983bac34d1b8262ed8da
alecburt <alec_burt@yahoo.com>
2025-03-03 02:32:53 +0100

    Update names.py

--- names.py
+++ names.py
@@ -1,26 +1,13 @@
+namesDictionary= {
+"alec": "Alec Burt",
+"ArmyAmarillo": "Alec Burt",
+"ari": "Ari Ordeix",
+"simon": "Simon Furness",
+"dad": "Norrie Burt"
+}
 
 def check_name(name):
-    if name == "alec":
-        team_name = name.replace("alec", "Alec Burt") 
-        return team_name
-    
-    if name == "ArmyAmarillo":
-        team_name = name.replace("ArmyAmarillo", "Alec Burt") 
-        return team_name
-        
-    if name == "ari":
-        team_name = name.replace("ari", "Ari Ordeix") 
-        return team_name
-
-    if name == "simon":
-        team_name = name.replace("simon", "Simon Furness")
-        return team_name
-    
-    if name == "dad":
-        team_name = name.replace("dad", "Norrie Burt")
-        return team_name
-    
+    if name in namesDictionary:
+        return namesDictionary[name]
     else:
-        team_name = name
-        return team_name
-
+        return name
