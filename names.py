0746c9a3390995beb99d2403403003772970290e
Alec Burt <alec_burt@yahoo.com>
2025-03-04 22:07:53 +0100

    Update names.py
    Now the name is formatted in the player_input, checked in calcs and updated in the new dictionary formatted_names.json

--- names.py
+++ names.py
@@ -1,13 +1,27 @@
-namesDictionary= {
-"alec": "Alec Burt",
-"ArmyAmarillo": "Alec Burt",
-"ari": "Ari Ordeix",
-"simon": "Simon Furness",
-"dad": "Norrie Burt"
-}
+import json
+
+""" Update the names each round. For the first week a blank dictionary is needed with just empty curly brackets in JSON."""
+
+with open(f'formatted_names.json', 'r') as names_json:
+    names_dict = json.load(names_json)
+    
+
+def format_name(name):
+    """In player_input_tk this takes the input name and if it is not in the formatted_names.json dictionary 
+    it will prompt to add the formatted name and add it to the dictionary"""
+    if name in names_dict:
+        return names_dict[name]
+    else:
+        print(f"{name} is not in the system!")
+        formatted_name = input("What is the formatted name? ")
+        names_dict.update({name: formatted_name})
+        with open(f'formatted_names.json', 'w') as ngw:
+            json.dump(names_dict, ngw, indent = 2)
+        return formatted_name
+
 
 def check_name(name):
-    if name in namesDictionary:
-        return namesDictionary[name]
+    if name in names_dict:
+        return names_dict[name]
     else:
         return name
