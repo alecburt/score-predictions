b82f9df51e43169f115b6066c02de4de8844009c
Alec Burt <alec_burt@yahoo.com>
2025-03-05 23:19:24 +0100

    Add files via upload
    This will sort the results.json data and print to terminal using the pretty tables library.

---
+++ table_pt.py
@@ -0,0 +1,23 @@
+import json
+from prettytable import PrettyTable 
+
+round = 4
+
+with open(f'round_{round}_results.json', 'r') as results_json:
+    table_data = json.load(results_json)
+
+# sort the table by total 
+sorted_table_data = sorted(table_data.items(), key=lambda x: x[1][4], reverse = True)    
+
+# create table
+score_prediction_table = PrettyTable(["Position", "Player", "Games Played", "Result", "Goalscorer", "Perfect", "Total"]) 
+  
+# add rows
+index = 0 
+for player_score in sorted_table_data:
+    index += 1
+    sorted_player_list = [index, player_score[0], player_score[1][0], player_score[1][1], player_score[1][2], player_score[1][3], player_score[1][4]]
+    score_prediction_table.add_row(sorted_player_list) 
+
+  
+print(score_prediction_table)
